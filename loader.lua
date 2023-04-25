630c5b08eb63b72e8617abe69290d96e325f8be5
Spring67#0001 <109059103+Springs67@users.noreply.github.com>
2023-04-25 19:49:53 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,13 @@
-local games = {
-    ["2512643572"] = "Bubble Gum Simulator"
-}
+local games = "Bubble Gum Simulator and Free Hatchers"
 
-local gameID = games[tostring(game.PlaceId)] or "Universal"
+if game.PlaceId == 11162791099 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Springs67/l54sHub/3kdIKfmsdSf/11162791099.lua"))()
+elseif game.PlaceId == 2512643572 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Springs67/l54sHub/3kdIKfmsdSf/2512643572.lua"))()
+else
+    game.StarterGui:SetCore("SendNotification", {
+		Title = "l54sHub";
+		Text = "game not supported. Games that ARE supported are '"..games.."'.";
+		Duration = 10;
+	})
+end
