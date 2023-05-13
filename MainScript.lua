afd12c5fbc3b12a78458faf93600edfb97c9dd4d
Spring67#0001 <109059103+Springs67@users.noreply.github.com>
2023-05-13 01:29:52 -0400

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,4 +1,4 @@
-local games = "BGS, Free Hatchers, and Treasure Chest Simulator"
+local games = "BGS, Free Hatchers, Treasure Chest Simulator, and Mine Blocks Simulator"
 
 if game.PlaceId == 11162791099 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Springs67/l54sHub/3kdIKfmsdSf/games/11162791099.lua"))()
@@ -6,6 +6,8 @@ elseif game.PlaceId == 2512643572 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Springs67/l54sHub/3kdIKfmsdSf/games/2512643572.lua"))()
 elseif game.PlaceId == 11922377983 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Springs67/l54sHub/3kdIKfmsdSf/games/11922377983.lua"))()
+elseif game.PlaceId == 13171269529 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Springs67/l54sHub/3kdIKfmsdSf/games/13171269529.lua"))()
 else
     game.StarterGui:SetCore("SendNotification", {
 		Title = "l54sHub";
