45abca1fdaf65ea949cfc2b4424af3ddf8daff71
Spring67#0001 <109059103+Springs67@users.noreply.github.com>
2023-05-12 23:43:20 -0400

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,9 +1,11 @@
-local games = "Bubble Gum Simulator and Free Hatchers"
+local games = "BGS, Free Hatchers, and Treasure Chest Simulator"
 
 if game.PlaceId == 11162791099 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Springs67/l54sHub/3kdIKfmsdSf/games/11162791099.lua"))()
 elseif game.PlaceId == 2512643572 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Springs67/l54sHub/3kdIKfmsdSf/games/2512643572.lua"))()
+elseif game.PlaceId == 11922377983 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Springs67/l54sHub/3kdIKfmsdSf/games/11922377983.lua"))()
 else
     game.StarterGui:SetCore("SendNotification", {
 		Title = "l54sHub";
